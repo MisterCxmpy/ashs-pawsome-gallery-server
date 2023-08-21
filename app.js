a19ea195cbba05fff32835c6b875ddcbc3aff7e9
MisterCxmpy <cosgunhussein@gmail.com>
2023-08-21 16:24:37 +0100

    .

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ require('dotenv').config()
 const app = express();
 const port = process.env.PORT;
 
-app.use(cors({ origin: 'http://localhost:5173' }))
+app.use(cors({ origin: 'https://ashs-pawsome-gallery.netlify.app' }))
 
 const drive = google.drive({
   version: 'v3',
