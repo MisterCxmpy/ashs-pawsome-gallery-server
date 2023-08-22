fec72c8f9d074a72a86abd6fe7ef6c595e287f98
MisterCxmpy <cosgunhussein@gmail.com>
2023-08-22 10:20:07 +0100

    update files

--- app.js
+++ app.js
@@ -20,19 +20,29 @@ const drive = google.drive({
 
 app.get('/getImages', async (req, res) => {
   try {
-    const response = await drive.files.list({
-      q: `'${process.env.ADDRESS}' in parents`,
-      fields: 'files(name, webViewLink)',
-    });
+    let allFiles = [];
 
-    const webViewLinks = response.data.files.map(item => {
-      const urlParts = item.webViewLink.split('/');
-      const fileId = urlParts[urlParts.length - 2];
-      const fileExtension = item.name.split('.').pop();
-      return { id: fileId, format: fileExtension };
-    });
-    
-    res.json(webViewLinks);
+    let nextPageToken = null;
+    do {
+      const response = await drive.files.list({
+        q: `'${process.env.ADDRESS}' in parents`,
+        fields: 'files(name, webViewLink), nextPageToken',
+        pageSize: 1000,
+        pageToken: nextPageToken,
+      });
+
+      const files = response.data.files.map(item => {
+        const urlParts = item.webViewLink.split('/');
+        const fileId = urlParts[urlParts.length - 2];
+        const fileExtension = item.name.split('.').pop();
+        return { id: fileId, format: fileExtension };
+      });
+
+      allFiles = allFiles.concat(files);
+      nextPageToken = response.data.nextPageToken;
+    } while (nextPageToken);
+
+    res.json(allFiles);
   } catch (error) {
     console.error(error);
     res.status(500).json({ error: 'An error occurred' });
